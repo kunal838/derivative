3a94efe394dd9e3450bbbd33dae79aace2c932e8
kunal patel <pkunal257@gmail.com>
2021-07-04 15:24:48 +0530

    derivative

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
-import {sqrt,derivative} from 'mathjs'
+
+import Nav from './components/Nav';
+import Derivate from './components/Derivate';
 
 
 function App() {
-  const [count, setCount] = React.useState(0);
-  const [value, setValue] = React.useState(0);
+  
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <input onChange={(e)=>{
-          setCount(e.target.value)
-          
-        }} placeholder="enter a expression"
-        type="string"></input>
-        <button onClick={()=>{
-         setValue(derivative(count,'x')) 
-        }}>derivate</button>
-        
-        {value.toString()}
+       <Nav/>
+       <Derivate/>
         
       </header>
       
